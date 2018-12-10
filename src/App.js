a0b6b0dd34e67b5bc3f96395222b6441cc036467
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-12-10 10:51:36 -0500

    adds initial router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Home from './Home';
+import About from './About';
+import Users from './Users';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// Router watches the URL for changes
+// Route renders components based on the specific URL
+// Link changes the URL
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className='App'>
+          <nav>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/users'>Users</Link>
+            </li>
+          </nav>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/users' component={Users} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home(props) {
+  return (
+    <div>
+      <h1>Home</h1>
+      <h3>Welcome!</h3>
+    </div>
+  );
+}
+
+export default Home;
